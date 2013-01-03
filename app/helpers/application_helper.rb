fbdc5b88d1d01d2e631eb6380dabf08fc1cfb451
Adam Mueller <adam@freerunningtechnologies.com>
2013-01-03 08:52:08 -0800

    Format the date nicely for Games as well.
    Also, fixed a minor bug with the previous date formatting. (Would only
    work for edit forms, not new ones.)
    
    Change-Id: I19d95d8a93224649fdf958b09a4307390654e809

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def app_name
     "Imperilment!"
   end
+
+  def format_date(date)
+    if date.nil?
+      ""
+    else
+      l date, format: :short
+    end
+  end
+  alias d format_date
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -5,4 +5,18 @@ describe ApplicationHelper do
     subject { helper.app_name }
     specify { should == 'Imperilment!' }
   end
+
+  describe '.format_date' do
+    subject { helper.format_date(date) }
+
+    context 'when the date is nil' do
+      let(:date) { nil }
+      it { should be_blank }
+    end
+
+    context 'when the date is not nil' do
+      let(:date) { DateTime.parse('1964-03-30') }
+      it { should == '1964-03-30' }
+    end
+  end
 end
