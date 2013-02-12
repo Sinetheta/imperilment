5a5023ad3b6be8d2f96a07c8397338d92c637b49
Adam Mueller <adam@freerunningtechnologies.com>
2013-02-12 14:59:09 -0800

    Redirect to google login if not logged in.
    Change-Id: I9d99b3f985573c843f07509a007dd28cacb05e2f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
       message = exception.message
-      format.html { render inline: %{<div class="alert alert-error alert-block"><h4>#{t('unauthorized.header')}</h4>#{message}</h1>}, layout: 'application' }
+      format.html { redirect_to user_omniauth_authorize_path(:google) }
       format.json { render json: {error: 'forbidden', message: message}, status: :forbidden }
       format.xml  { render xml:  {error: 'forbidden', message: message}, status: :forbidden }
     end
