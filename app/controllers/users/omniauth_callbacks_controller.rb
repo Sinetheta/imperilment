592fd6a3a4b94cc0bae4615f232d30a9fbcd84d9
Richard Wilson <richard@freerunningtech.com>
2013-11-10 16:40:58 -0800

    Remember login by default.
    Change-Id: Id4ff9993bc7af3babb9091247e8987d469b28210

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,8 +1,12 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  include Devise::Controllers::Rememberable
+
   skip_before_filter :verify_authenticity_token, :only => [:google]
 
   def google
     @user = User.find_for_open_id(request.env["omniauth.auth"], current_user)
+
+    remember_me(@user)
 
     flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
     sign_in_and_redirect @user, :event => :authentication
