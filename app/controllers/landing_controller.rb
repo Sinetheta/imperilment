d17ca953c59b6d27115283b2729832915654fb27
John Hawthorn <john.hawthorn@gmail.com>
2015-01-15 00:07:09 -0800

    redirect to the first unanswered question from /
    Change-Id: Ib0fc45298ff754aeb3176fdebe646fdc5d509520

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,7 +2,8 @@ class LandingController < ApplicationController
   skip_authorization_check
 
   def show
-    answer = Answer.most_recent
+    answer = current_user.pending_answers.first if current_user
+    answer ||= Answer.most_recent
     if answer
       redirect_to game_answer_path(answer.game, answer)
     else
