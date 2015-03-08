a48fef270b31acec1e329f5870f88e0eee777eba
Sinetheta <k.attfield@gmail.com>
2015-03-07 22:04:15 -0800

    Add new Web Hook form
    Redirect back to index if successful. A #show has no value.
    
    Change-Id: Icd99b12bd8a82318055bab20b1578fc250a9b2f3

--- app/controllers/web_hooks_controller.rb
+++ app/controllers/web_hooks_controller.rb
@@ -7,4 +7,24 @@ class WebHooksController < ApplicationController
     @web_hooks = @web_hooks.order(active: :desc, created_at: :desc)
     respond_with(@web_hooks)
   end
+
+  def new
+    @web_hook = WebHook.new
+    respond_with(@web_hook)
+  end
+
+  def create
+    @web_hook = WebHook.new(web_hook_params)
+    if @web_hook.save
+      flash.notice = t :model_create_successful, model: WebHook.model_name.human
+      redirect_to action: :index
+    else
+      respond_with @web_hook
+    end
+  end
+
+  private
+  def web_hook_params
+    params.require(:web_hook).permit(:url, :active)
+  end
 end
--- spec/controllers/web_hooks_controller_spec.rb
+++ spec/controllers/web_hooks_controller_spec.rb
@@ -4,12 +4,52 @@ describe WebHooksController do
   authorize
 
   let!(:web_hook) { create :web_hook }
-  let(:default_params) { { web_hook: { url: 'http://www.example.com/fake_hook', active: true } } }
 
   describe "GET index" do
     it "assigns all web hooks as @web_hooks" do
       get :index
       expect(assigns(:web_hooks)).to eq([web_hook])
+    end
+  end
+
+  describe "GET new" do
+    it "assigns a new web_hook as @web_hook" do
+      get :new
+      expect(assigns(:web_hook)).to be_a_new(WebHook)
+    end
+  end
+
+  describe "POST create" do
+    let(:default_params) { { web_hook: { url: 'http://www.example.com/fake_hook', active: true } } }
+    subject(:valid_hook) { post :create, default_params }
+
+    describe "with valid params" do
+      it "creates a new WebHook" do
+        expect{ valid_hook }.to change(WebHook, :count).by(1)
+      end
+
+      it "assigns a newly created web hook as @web_hook" do
+        valid_hook
+        expect(assigns(:web_hook)).to be_a(WebHook)
+        expect(assigns(:web_hook)).to be_persisted
+      end
+
+      it "redirects to the web hook list" do
+        expect(valid_hook).to redirect_to(web_hooks_url)
+      end
+    end
+
+    describe "with invalid params" do
+      subject(:invalid_hook) { post :create, web_hook: { url: 'nope' } }
+
+      it "assigns a newly created but unsaved web hook as @web_hook" do
+        invalid_hook
+        expect(assigns(:web_hook)).to be_a(WebHook)
+      end
+
+      it "re-renders the 'new' template" do
+        expect(invalid_hook).to render_template("new")
+      end
     end
   end
 end
