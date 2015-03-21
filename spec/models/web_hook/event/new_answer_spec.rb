6910bff64a41c021a5d93695139dfce757629c4a
Sinetheta <k.attfield@gmail.com>
2015-03-21 14:30:50 -0700

    Add "new answer" webhook event
    The payload for our transmission.
    
    Change-Id: Idc2532805e75abcf03c402125434a6dced938380

---
+++ spec/models/web_hook/event/new_answer_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe WebHook::Event::NewAnswer do
+  let(:answer) { FactoryGirl.create :answer, correct_question: 'what is fail?', category_name: 'Rails RSpec' }
+  let(:event) { WebHook::Event::NewAnswer.new answer }
+
+  describe 'serialize' do
+    subject { event.serialize }
+
+    it 'contains a link back to the answer' do
+      expect(subject).to include('http://test.com/games/1/answers/1')
+    end
+
+    it 'does not contain the correct response' do
+      expect(subject).not_to include('what is fail?')
+    end
+
+    it "contains the answer's category name" do
+      expect(subject).to include('Rails RSpec')
+    end
+  end
+end
