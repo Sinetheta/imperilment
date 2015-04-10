080d1cf6a243525d277ad7e6152e0677be49a003
Sinetheta <k.attfield@gmail.com>
2015-04-09 23:09:33 -0700

    Add application helper for injecting svgs inline
    Change-Id: I20689720c80141b8cacdab7fa664a08bebe1d229

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,10 @@ module ApplicationHelper
   def render_markdown(text)
     raw Redcarpet::Markdown.new(Redcarpet::Render::HTML.new).render(text)
   end
+
+  def inline_svg(path)
+    File.open("app/assets/images/#{path}", 'rt') do |file|
+      file.read.html_safe
+    end
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
+def md5_hash(str)
+  Digest::MD5.hexdigest(str)
+end
+
 describe ApplicationHelper do
   describe '.app_name' do
     subject { helper.app_name }
@@ -24,6 +28,18 @@ describe ApplicationHelper do
     it 'should call will_paginate' do
       expect(helper).to receive(:will_paginate)
       helper.paginate(OpenStruct.new)
+    end
+  end
+
+  describe 'inline_svg' do
+    subject { inline_svg('imperilment-logo.svg') }
+
+    it 'returns the file contents' do
+      expect(md5_hash subject).to eq('dcd29a61e7e81998f263be03b7e1f99c')
+    end
+
+    it 'returns a safe string' do
+      expect(subject.html_safe?).to be(true)
     end
   end
 end
