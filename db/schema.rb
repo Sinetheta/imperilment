fae1267ad26614140a39a56b87a92920b3df30fc
Josef Waller <josef@siriusapplications.com>
2017-07-26 14:21:02 -0700

    Allow admins to mark a user as in office or not
    Add a checkbox in the edit user page (`'admin/users/`) which toggles
    `User.in_office`, a boolean representing whether the user is in office
    or not. Default value is false.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150307173552) do
+ActiveRecord::Schema.define(version: 20170726210225) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "game_id"
@@ -76,8 +76,8 @@ ActiveRecord::Schema.define(version: 20150307173552) do
   add_index "roles", ["name"], name: "index_roles_on_name"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20150307173552) do
     t.datetime "updated_at"
     t.string   "first_name"
     t.string   "last_name"
+    t.boolean  "in_office",              default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
