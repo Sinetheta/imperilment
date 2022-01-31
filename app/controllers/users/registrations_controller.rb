62cc13de183b7ce1a6515e2fed95504154c6039a
Kevin Attfield <k.attfield@gmail.com>
2022-01-30 23:33:00 -0800

    Do not require password confirmation to update user
    Google authed users don't know their password since we generate
    it for them. It's also not easy to distinguish those accounts
    from those that use the login form.
    
    Although it is possible to reset your password, that's not the
    most intuitive action when confronted with this user edit page
    for the first time. For now, I would like to drop password
    confirmation to ease adtoption of the new display_name.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def account_update_params
-    params.require(:user).permit(:email, :display_name, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:email, :display_name, :password, :password_confirmation)
+  end
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
   end
 end
--- spec/controllers/users/registrations_controller_spec.rb
+++ spec/controllers/users/registrations_controller_spec.rb
@@ -6,8 +6,8 @@ describe Users::RegistrationsController do
 
     authorize_and_login
 
-    it "allows a user to update their display_name" do
-      put :update, params: { user: { display_name: 'new username', current_password: user.password } }
+    it "allows a user to update their display_name without a password confirmation" do
+      put :update, params: { user: { display_name: 'new username' } }
       expect(response).to redirect_to('/')
       expect(user.reload.display_name).to eq('new username')
     end
